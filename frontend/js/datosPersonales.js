7f492d32971780fea0542bc6d3a7df397422c01e
Odal Sigel <cornagopablo@gmail.com>
2024-06-12 15:05:06 +0200

    Presentación basica de los datos del usuario

---
+++ frontend/js/datosPersonales.js
@@ -0,0 +1,18 @@
+const $nombre = document.getElementById("nombre");
+const $especialidad = document.getElementById("especialidad");
+const $imagen = document.getElementById("imagen");
+const $avatar = document.getElementById("avatar");
+
+$.ajax({
+    url: "../backend/index.php",
+    method: "GET",
+    success: function (respuesta) {
+        respuesta = JSON.parse(respuesta);
+        const usuario = respuesta.usuario;
+
+        $nombre.textContent = usuario.nombre + " " + usuario.apellido;
+        $especialidad.textContent = usuario.especialidad;
+        $imagen.setAttribute("src", usuario.foto);
+        $avatar.setAttribute("src", usuario.avatar);
+    },
+});
